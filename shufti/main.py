4b261f871d194724490efd3685a5149e00646d99
benvilliere <benoit.villiere@icloud.com>
2023-10-03 17:32:10 +0100

    Handling binary files

--- shufti/main.py
+++ shufti/main.py
@@ -5,40 +5,64 @@ from file_extension_mappings import FILE_EXTENSION_MAPPINGS
 
 DEFAULT_EXCLUDED_DIRECTORIES = ['node_modules', '.git', '__pycache__']
 DEFAULT_EXCLUDED_FILES = ['.gitattributes', '.gitignore', 'LICENSE', 'README.md']
+BINARY_FILE_EXTENSIONS = [
+    '.exe', '.dll', '.bin', '.o', '.a', '.lib', 
+    '.so', '.dylib', '.bmp', '.gif', '.jpg', '.jpeg', 
+    '.png', '.webp', '.mp3', '.wav', '.ogg', '.mp4', 
+    '.webm', '.zip', '.tar', '.gz', '.bz2', '.7z', 
+    '.rar', '.pdf', '.doc', '.docx', '.xls', '.xlsx', 
+    '.ppt', '.pptx', '.odt', '.ods'
+]
 
-def display_files_in_directory(directory, depth=0, limit=500, depth_limit=None, excluded_items=[]):
+def is_binary_file(filename):
+    return os.path.splitext(filename)[1] in BINARY_FILE_EXTENSIONS
+
+def display_files_in_directory(directory, depth=0, limit=500, depth_limit=None, excluded_items=[], included_items=[], include_binary=False):
     output = ""
 
+    # Stop if we've reached or exceeded the depth limit
     if depth_limit is not None and depth >= depth_limit:
         return ""
 
     for item in os.listdir(directory):
-        if item in excluded_items:
+        if item in excluded_items and item not in included_items:
             continue
 
         item_path = os.path.abspath(os.path.join(directory, item))
 
         if os.path.isdir(item_path):
             output += "  " * depth + f"/{item}:\n"
-            output += display_files_in_directory(item_path, depth+1, limit, depth_limit, excluded_items)
+            output += display_files_in_directory(item_path, depth+1, limit, depth_limit, excluded_items, included_items, include_binary)
         else:
-            with open(item_path, 'r') as file:
-                content = file.read() if limit is None else file.read(limit + 1)
-                truncated = False if limit is None else len(content) > limit
-                content = content[:limit]
-                
-                file_output = "  " * depth + f"-- {item}:\n"
-                if content:
-                    language = FILE_EXTENSION_MAPPINGS.get(os.path.splitext(item)[1], '')
-                    file_output += (
-                        "```" + language + "\n" +
-                        content +
-                        ("...[more content...]" if truncated else "") + "\n" +
-                        "```\n"
-                    )
+            if is_binary_file(item):
+                if include_binary:
+                    file_output = "  " * depth + f"-- {item}:\n"
+                    file_output += "[Binary File]\n"
                 else:
-                    file_output += "[Empty File]\n"
-                output += file_output
+                    continue
+            else:
+                with open(item_path, 'r') as file:
+                    try:
+                        content = file.read(limit + 1) if limit else file.read()
+                        truncated = bool(limit) and len(content) > limit
+                        content = content[:limit]
+
+                        file_output = "  " * depth + f"-- {item}:\n"
+                        if content:
+                            language = FILE_EXTENSION_MAPPINGS.get(os.path.splitext(item)[1], '')
+                            file_output += (
+                                "```" + language + "\n" +
+                                content +
+                                ("...[more content...]" if truncated else "") + "\n" +
+                                "```\n"
+                            )
+                        else:
+                            file_output += "[Empty File]\n"
+                    except UnicodeDecodeError:
+                        # In case the binary check misses some files
+                        file_output = "  " * depth + f"-- {item}:\n"
+                        file_output += "[Binary File]\n"
+            output += file_output
 
     return output
 
@@ -54,21 +78,24 @@ def main():
         help="Comma separated list of files or directories to exclude.")
     parser.add_argument('-i', '--include', type=str, default="", 
         help="Comma separated list of files or directories to forcefully include even if they are in the exclude list.")
+    parser.add_argument('-b', '--binary', action="store_true", 
+        help="Include binary files with a [Binary File] flag.")
     
     args = parser.parse_args()
 
-    # Combine default excluded items and user-provided excluded items
     excluded_items = DEFAULT_EXCLUDED_FILES + DEFAULT_EXCLUDED_DIRECTORIES
     if args.exclude:
         excluded_items += args.exclude.split(",")
     if args.include:
         included_items = args.include.split(",")
         excluded_items = [item for item in excluded_items if item not in included_items]
+    if not args.binary:
+        excluded_items += [f for f in BINARY_FILE_EXTENSIONS]
 
     absolute_path = os.path.abspath(args.path)
 
     if os.path.exists(absolute_path):
-        output = display_files_in_directory(absolute_path, limit=args.limit, depth_limit=args.depth, excluded_items=excluded_items)
+        output = display_files_in_directory(absolute_path, limit=args.limit, depth_limit=args.depth, excluded_items=excluded_items, include_binary=args.binary)
         if args.copy:
             pyperclip.copy(output)
         else:
